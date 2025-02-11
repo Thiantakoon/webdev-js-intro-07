55e625b6d1a6e8f9c7557efd0037d12b8055f012
Your Name <you@example.com>
2025-02-11 12:54:00 -0500

    updated assignment.js,updated index.js,updated Readme.rd

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,27 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+function returnTheRemainder (a,b){
+    return a % b;
+}
+console.log( Remainder);
+
+function checkIfEven(number){
+if (number % 2 === 0){
+return true;
+}
+else {
+    return false;
+}
+    
+  }
+    
+
+function getTheFourthElement(lostNumbers){
+ for (let i =0;i.length>4;i++){
+ 
+ }
+}
+  
+  
+    
--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@
 // that are not declared
 "use strict"
 
+function farewell(Goodbye) {
+    console.log( 'Goodbye,' +'!');
+}
+const farewell = (Goodbye)=> {
+    return 'Goodbye,'+ '!';
+};
+function farewell(name='Friend') {
+    console.log( 'Goodbye,' +name +'!');
+    farewell(John);
+}
+//
